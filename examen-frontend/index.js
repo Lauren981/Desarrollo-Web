17985494a204452fefa4a3c64c820d0f3e9f7f38
Lauren <lauren.flores@unitec.edu>
2025-05-23 21:00:30 -0600

    fix

--- examen-frontend/index.js
+++ examen-frontend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { where } = require('sequelize')
-const Gasto = require('./Models/Gasto')
+const Gasto = require('./src/Models/Gasto')
 
 var cors = require('cors')
 
